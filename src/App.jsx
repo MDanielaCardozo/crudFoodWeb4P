4b3725cbd63ea7158731d55e0df204645538f909
Daniela Cardozo <mdanielacardozo@gmail.com>
2025-10-22 21:36:42 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
+import Menu from './components/shared/Menu'
+import Footer from './components/shared/Footer'
+import Home from './components/pages/Home'
 
 function App() {
 
 
   return (
     <>
-    
+    <BrowserRouter>
+    <Menu></Menu>
+    <main>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </main>
+    <Footer></Footer>
+    </BrowserRouter>
     </>
   )
 }
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,9 +1,11 @@
 const Footer = () => {
-      return (
+  return (
     <>
-    
+      <footer className="bg-warning text-dark py-4 text-center">
+        <p>&copy;Todos los derechos reservados.</p>
+      </footer>
     </>
-  )
-}
+  );
+};
 
 export default Footer;
